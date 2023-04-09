606d5c82b014d63230cce71cc6574d5bb4f226c3
quocvu <0468201130@caothang.edu.vn>
2023-04-09 19:37:32 +0700

    9/4/2023

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{asset('asset/css/style.css')}}">
+  <title>Little</title>
+</head>
+
+<body>
+  <section id="main">
+    <header>
+      <nav>
+        <ul>
+          <img srcset="{{ asset('./asset/images/Logo.png 3x') }}">
+          <div class="nav-list">
+            <li><a href="http://">Trang Chủ</a></li>
+            <li><a href="http://">Sự Kiện</a></li>
+            <li><a href="http://">Liên Hệ</a></li>
+          </div>
+          <div class="right">
+            <img srcset="{{ asset('./asset/images/logo_phone.png 2x') }}">
+            <li>0938342942</li>
+          </div>
+
+        </ul>
+      </nav>
+    </header>
+
+
+    <section class="content">
+     
+      <div class="">
+      
+      </div>
+    </section>
+  </section>
+</body>
+
+</html>
